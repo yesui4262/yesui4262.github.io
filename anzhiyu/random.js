e87973f47bdd9df5b77b6d41be6cfefe2f6fe811
yesui4262 <3463353981@qq.com>
2024-04-01 18:17:29 +0800

    Site updated: 2024-04-01 18:17:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/03/24/60s读懂世界(日更)/","2024/03/17/关于评论/","2024/03/09/第1个博客/"];function toRandomPost(){
+var posts=["2024/03/17/关于评论/","2024/03/09/第1个博客/","2024/03/24/60s读懂世界(日更)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
